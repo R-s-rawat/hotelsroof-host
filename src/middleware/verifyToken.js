094452e502856a541410aaab01b7a1576b50b9fe
R-s-rawat <rohitrawat590590@gmail.com>
2025-05-15 07:34:12 +0530

    checking if we getting correct REQ from frontend

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -5,6 +5,7 @@ const JWT_SECRET = process.env.JWT_SECRET_KEY;
 const verifyToken = (req, res, next) => {
     try{
         const token = req.cookies.token; 
+        console.log("your request that we received", req);
         // console.log('Token from cookie:', token);
         //const token = req.headers.authorization?.split(' ')[1]; //Bearer token(testing oK)
         if (!token) {
